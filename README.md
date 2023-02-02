6272fec410c5f1f20797da0167760978ad286fa7
votrepoesie <vrli@uchicago.edu>
2023-02-02 14:02:08 -0600

    4 divs change color

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# non-objective-composition
+# non-objective composition
 
-2/2/23
+(2/2/23)
 
 this is inspired by abstract art, especially mondrian's meticulus compositions 
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
 //this a dumb how do I condense this code
+//c1
 
-function changeColor() {
+function changeColor1() {
     const r = Math.random() * 255
     const g = Math.random() * 255
     const b = Math.random() * 255
@@ -19,13 +20,13 @@ function changeBack() {
   
 const c1 = document.getElementsByClassName('c1');
 for (let i = 0; i < c1.length; i++) {
-    c1[i].addEventListener('mouseover', changeColor);
+    c1[i].addEventListener('mouseover', changeColor1);
     c1[i].addEventListener('mouseout', changeBack)
 }
 
-//--------------------------------
+//c2
 
-function changeColor1() {
+function changeColor2() {
     const r = Math.random() * 255
     const g = Math.random() * 255
     const b = Math.random() * 255
@@ -39,9 +40,47 @@ function changeColor1() {
 
 const c2 = document.getElementsByClassName('c2');
 for (let i = 0; i < c2.length; i++) {
-    c2[i].addEventListener('mouseover', changeColor1);
+    c2[i].addEventListener('mouseover', changeColor2);
     c2[i].addEventListener('mouseout', changeBack)
 }
   
 
+//c3
 
+function changeColor3() {
+    const r = Math.random() * 255
+    const g = Math.random() * 255
+    const b = Math.random() * 255
+    const randomColor = `rgb(${r}, ${g}, ${b})`
+    const c3 = document.getElementsByClassName('c3');
+    for (let i = 0; i < c3.length; i++) {
+        c3[i].style.backgroundColor = randomColor;
+    }
+    this.style.backgroundColor = randomColor;
+}
+
+const c3 = document.getElementsByClassName('c3');
+for (let i = 0; i < c2.length; i++) {
+    c3[i].addEventListener('mouseover', changeColor3);
+    c3[i].addEventListener('mouseout', changeBack)
+}
+
+//c4
+
+function changeColor4() {
+    const r = Math.random() * 255
+    const g = Math.random() * 255
+    const b = Math.random() * 255
+    const randomColor = `rgb(${r}, ${g}, ${b})`
+    const c4 = document.getElementsByClassName('c4');
+    for (let i = 0; i < c4.length; i++) {
+        c4[i].style.backgroundColor = randomColor;
+    }
+    this.style.backgroundColor = randomColor;
+}
+
+const c4 = document.getElementsByClassName('c4');
+for (let i = 0; i < c4.length; i++) {
+    c4[i].addEventListener('mouseover', changeColor4);
+    c4[i].addEventListener('mouseout', changeBack)
+}
