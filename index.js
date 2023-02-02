07f6b8c323f39b6cfb63a87bf29ec6b4b29a513a
votrepoesie <vrli@uchicago.edu>
2023-02-02 16:50:54 -0600

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-//this a dumb how do I condense this code
+//how do I condense this code
 //https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementsByClassName 
 
 //c1
@@ -16,11 +16,14 @@ function changeColor1() {
     this.style.backgroundColor = randomColor;
 }
 
+//noticed that if declare randomColor outside the functions then every cube changes to the same color 
+//but maybe I'm just doing something wrong
+
 function changeBack() {
     function primaryColors(items) {
         return items [Math.floor(Math.random() * items.length)]; 
     }
-    const items = ['#ff0101', '#fff001', '#0101fd', '#30303a', '#f9f9f9']
+    const items = ['#ff0101', '#fff001', '#0101fd', '#30303a', '#f9f9f9', '#f9f9f9', '#f9f9f9']
     this.style.backgroundColor = primaryColors(items);
 }
   
@@ -106,14 +109,171 @@ function changeColor5() {
 }
 
 const c5 = document.getElementsByClassName('c5');
-for (let i = 0; i < c4.length; i++) {
+for (let i = 0; i < c5.length; i++) {
     c5[i].addEventListener('mouseover', changeColor5);
     c5[i].addEventListener('mouseout', changeBack)
 }
 
 //c6
 
+function changeColor6() {
+    const r = Math.random() * 255
+    const g = Math.random() * 255
+    const b = Math.random() * 255
+    const randomColor = `rgb(${r}, ${g}, ${b})`
+    const c6 = document.getElementsByClassName('c6');
+    for (let i = 0; i < c6.length; i++) {
+        c6[i].style.backgroundColor = randomColor;
+    }
+    this.style.backgroundColor = randomColor;
+}
+
+const c6 = document.getElementsByClassName('c6');
+for (let i = 0; i < c6.length; i++) {
+    c6[i].addEventListener('mouseover', changeColor6);
+    c6[i].addEventListener('mouseout', changeBack)
+}
 
 //c7
+
+function changeColor7() {
+    const r = Math.random() * 255
+    const g = Math.random() * 255
+    const b = Math.random() * 255
+    const randomColor = `rgb(${r}, ${g}, ${b})`
+    const c7 = document.getElementsByClassName('c7');
+    for (let i = 0; i < c7.length; i++) {
+        c7[i].style.backgroundColor = randomColor;
+    }
+    this.style.backgroundColor = randomColor;
+}
+
+const c7 = document.getElementsByClassName('c7');
+for (let i = 0; i < c7.length; i++) {
+    c7[i].addEventListener('mouseover', changeColor7);
+    c7[i].addEventListener('mouseout', changeBack)
+}
+
 //c8
 
+function changeColor8() {
+    const r = Math.random() * 255
+    const g = Math.random() * 255
+    const b = Math.random() * 255
+    const randomColor = `rgb(${r}, ${g}, ${b})`
+    const c8 = document.getElementsByClassName('c8');
+    for (let i = 0; i < c7.length; i++) {
+        c8[i].style.backgroundColor = randomColor;
+    }
+    this.style.backgroundColor = randomColor;
+}
+
+const c8 = document.getElementsByClassName('c8');
+for (let i = 0; i < c8.length; i++) {
+    c8[i].addEventListener('mouseover', changeColor8);
+    c8[i].addEventListener('mouseout', changeBack)
+}
+
+//9
+
+function changeColor9() {
+    const r = Math.random() * 255
+    const g = Math.random() * 255
+    const b = Math.random() * 255
+    const randomColor = `rgb(${r}, ${g}, ${b})`
+    const c9 = document.getElementsByClassName('c9');
+    for (let i = 0; i < c7.length; i++) {
+        c9[i].style.backgroundColor = randomColor;
+    }
+    this.style.backgroundColor = randomColor;
+}
+
+const c9 = document.getElementsByClassName('c9');
+for (let i = 0; i < c8.length; i++) {
+    c9[i].addEventListener('mouseover', changeColor9);
+    c9[i].addEventListener('mouseout', changeBack)
+}
+
+//10
+
+function changeColor10() {
+    const r = Math.random() * 255
+    const g = Math.random() * 255
+    const b = Math.random() * 255
+    const randomColor = `rgb(${r}, ${g}, ${b})`
+    const c10 = document.getElementsByClassName('c10');
+    for (let i = 0; i < c7.length; i++) {
+        c10[i].style.backgroundColor = randomColor;
+    }
+    this.style.backgroundColor = randomColor;
+}
+
+const c10 = document.getElementsByClassName('c10');
+for (let i = 0; i < c10.length; i++) {
+    c10[i].addEventListener('mouseover', changeColor10);
+    c10[i].addEventListener('mouseout', changeBack)
+}
+
+//11
+
+function changeColor11() {
+    const r = Math.random() * 255
+    const g = Math.random() * 255
+    const b = Math.random() * 255
+    const randomColor = `rgb(${r}, ${g}, ${b})`
+    const c11 = document.getElementsByClassName('c11');
+    for (let i = 0; i < c7.length; i++) {
+        c11[i].style.backgroundColor = randomColor;
+    }
+    this.style.backgroundColor = randomColor;
+}
+
+const c11 = document.getElementsByClassName('c11');
+for (let i = 0; i < c8.length; i++) {
+    c11[i].addEventListener('mouseover', changeColor11);
+    c11[i].addEventListener('mouseout', changeBack)
+}
+
+
+//12
+
+function changeColor12() {
+    const r = Math.random() * 255
+    const g = Math.random() * 255
+    const b = Math.random() * 255
+    const randomColor = `rgb(${r}, ${g}, ${b})`
+    const c12 = document.getElementsByClassName('c12');
+    for (let i = 0; i < c7.length; i++) {
+        c12[i].style.backgroundColor = randomColor;
+    }
+    this.style.backgroundColor = randomColor;
+}
+
+const c12 = document.getElementsByClassName('c12');
+for (let i = 0; i < c12.length; i++) {
+    c12[i].addEventListener('mouseover', changeColor12);
+    c12[i].addEventListener('mouseout', changeBack)
+}
+
+//13
+
+function changeColor13() {
+    const r = Math.random() * 255
+    const g = Math.random() * 255
+    const b = Math.random() * 255
+    const randomColor = `rgb(${r}, ${g}, ${b})`
+    const c13 = document.getElementsByClassName('c13');
+    for (let i = 0; i < c7.length; i++) {
+        c13[i].style.backgroundColor = randomColor;
+    }
+    this.style.backgroundColor = randomColor;
+}
+
+const c13 = document.getElementsByClassName('c13');
+for (let i = 0; i < c13.length; i++) {
+    c13[i].addEventListener('mouseover', changeColor13);
+    c13[i].addEventListener('mouseout', changeBack)
+}
+
+//14
+//15
